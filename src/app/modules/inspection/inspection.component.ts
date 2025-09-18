cf36279570fdc1276987d6c7a496d8bb6d812db8
Nigel Makwara <nigel@epione.net>
2025-09-18 20:49:55 +0200

    added render inspection form logic

--- src/app/modules/inspection/inspection.component.ts
+++ src/app/modules/inspection/inspection.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable, switchMap, take, takeUntil, tap } from 'rxjs';
 import { InspectionJson, Line } from '../../models/inspection.model';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { HasObservablesDirective } from '../../directives/has-observables.directive';
 
 @Component({
@@ -16,17 +16,22 @@ import { HasObservablesDirective } from '../../directives/has-observables.direct
 export class InspectionComponent extends HasObservablesDirective implements OnInit {
 
   public inspectionTypeControl = new FormControl<string>('rental_quality_check_inspection');
+  public inspectionForm = new FormGroup({});
+  public formFields: Array<{id: string, name: string, condition_options: any[]}> = [];
 
   private readonly http: HttpClient = inject(HttpClient);
 
   ngOnInit(): void {
-    this.requestInspectionDataObservable(this.inspectionTypeControl.value!).pipe(take(1)).subscribe(); //initial json file load
+    this.requestInspectionDataObservable(this.inspectionTypeControl.value!)
+      .pipe(take(1))
+      .subscribe(lines => this.renderInspectionLines(lines)); //initial json file load
 
     this.inspectionTypeControl.valueChanges
       .pipe(
+        tap(console.log),
         takeUntil(this.destroy$),
         switchMap(jsonName => this.requestInspectionDataObservable(jsonName!))
-      ).subscribe();
+      ).subscribe(lines => this.renderInspectionLines(lines));
   }
 
   private requestInspectionDataObservable(path: string): Observable<Line[]> { //TODO: to be refactored to a URL (-_-)
@@ -36,6 +41,24 @@ export class InspectionComponent extends HasObservablesDirective implements OnIn
           return res.result.lines;
         })
       );
+  }
+
+  private renderInspectionLines(lines: Line[]): void {
+
+    this.formFields = []; // Clear existing fields
+    this.inspectionForm = new FormGroup({});
+
+    lines.forEach(line => {
+      this.inspectionForm.addControl(line.id.toString(), new FormControl(line.name));
+      if (line.condition_options) {
+        this.formFields.push({
+          id: line.id.toString(),
+          name: line.name,
+          condition_options: line.condition_options
+        });
+      }
+    });
+
   }
 
 }
