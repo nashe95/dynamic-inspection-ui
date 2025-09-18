a2a19cdd9a9ff61244d74c6ba0d11e517765bea1
Nigel Makwara <nigel@epione.net>
2025-09-18 18:01:52 +0200

    added has observable directive

---
+++ src/app/directives/has-observables.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HasObservablesDirective } from './has-observables.directive';
+
+describe('HasObservablesDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HasObservablesDirective();
+    expect(directive).toBeTruthy();
+  });
+});
