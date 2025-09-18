50f4610ea895a576e504a5f59016b0eef9c68876
Nigel Makwara <nigel@epione.net>
2025-09-18 20:02:30 +0200

    added initial load for the json file

--- src/app/modules/inspection/inspection.component.ts
+++ src/app/modules/inspection/inspection.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable, switchMap, takeUntil, tap } from 'rxjs';
+import { map, Observable, switchMap, take, takeUntil, tap } from 'rxjs';
 import { InspectionJson, Line } from '../../models/inspection.model';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { HasObservablesDirective } from '../../directives/has-observables.directive';
@@ -15,14 +15,15 @@ import { HasObservablesDirective } from '../../directives/has-observables.direct
 })
 export class InspectionComponent extends HasObservablesDirective implements OnInit {
 
-  public inspectionTypeControl = new FormControl<string>('rental_quality_check');
+  public inspectionTypeControl = new FormControl<string>('rental_quality_check_inspection');
 
   private readonly http: HttpClient = inject(HttpClient);
 
   ngOnInit(): void {
+    this.requestInspectionDataObservable(this.inspectionTypeControl.value!).pipe(take(1)).subscribe(); //initial json file load
+
     this.inspectionTypeControl.valueChanges
       .pipe(
-        // tap(console.log),
         takeUntil(this.destroy$),
         switchMap(jsonName => this.requestInspectionDataObservable(jsonName!))
       ).subscribe();
