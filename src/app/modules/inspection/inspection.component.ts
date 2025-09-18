a9571f4567253046642f8bc3e133b69743f7d8e4
Nigel Makwara <nigel@epione.net>
2025-09-18 18:22:55 +0200

    added inspection component and routing

---
+++ src/app/modules/inspection/inspection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inspection',
+  imports: [],
+  templateUrl: './inspection.component.html',
+  styleUrl: './inspection.component.scss'
+})
+export class InspectionComponent {
+
+}
