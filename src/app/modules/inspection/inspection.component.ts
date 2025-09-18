ee936a3f1e6000546502710428b25610d88c59f3
Nigel Makwara <nigel@epione.net>
2025-09-18 19:57:33 +0200

    added switch inspection type logic

--- src/app/modules/inspection/inspection.component.ts
+++ src/app/modules/inspection/inspection.component.ts
@@ -1,25 +1,40 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
-import { InspectionJson } from '../../models/inspection.model';
+import { map, Observable, switchMap, takeUntil, tap } from 'rxjs';
+import { InspectionJson, Line } from '../../models/inspection.model';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { HasObservablesDirective } from '../../directives/has-observables.directive';
 
 @Component({
   selector: 'app-inspection',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './inspection.component.html',
   styleUrl: './inspection.component.scss'
 })
-export class InspectionComponent implements OnInit {
+export class InspectionComponent extends HasObservablesDirective implements OnInit {
+
+  public inspectionTypeControl = new FormControl<string>('rental_quality_check');
 
   private readonly http: HttpClient = inject(HttpClient);
 
   ngOnInit(): void {
-    this.http.get<InspectionJson>('../../data/rental_quality_check_inspection.json')
+    this.inspectionTypeControl.valueChanges
       .pipe(
-        map((res: any) => {
+        // tap(console.log),
+        takeUntil(this.destroy$),
+        switchMap(jsonName => this.requestInspectionDataObservable(jsonName!))
+      ).subscribe();
+  }
+
+  private requestInspectionDataObservable(path: string): Observable<Line[]> { //TODO: to be refactored to a URL (-_-)
+    return this.http.get<InspectionJson>(`../../data/${ path }.json`)
+      .pipe(
+        map(res => {
           return res.result.lines;
         })
-      ).subscribe((res) => console.log(res));
+      );
   }
 
 }
